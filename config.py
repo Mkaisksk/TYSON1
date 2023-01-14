a56e394610696c920b12073bbf5ef2c5405f31f4
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-01-14 17:56:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,20 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
 APP_ID = os.environ.get("APP_ID",
-"17053751")
+"حط ايبي ايدي الي تريد تنصبله هنا")
 APP_HASH = os.environ.get("APP_HASH",
-"6f90a1382848f5cc767a623f898ab23f")
+"حط ايبي هاش الي تريد تنصبله هنا")
 BOT_USERNAME = os.environ.get("BOT_USERNAME",
-                           "Qpqpa_bot")
+                           "حط يوزر بوت الي تريد تنصبله بدون @")
 session = os.environ.get("TERMUX",
-                        "AQEEODcAgJ2YhFOVyw4Sr_3wYXFzgIxdYtovgbPqHTaVXlp286F5i2vEPtem35I-HijIb77NNv3TLRd0Y4K_rQXYBCmTrD0tytqYKjdlupQ2AicZz5Lj5fkyNsVNdtjAWJgROyZkHhyYOJtWNvFeu94AFx8t1t0xi9prHYKS9bNeTTBneSeBNe8uSP9IZfNYmB2C1kSzwFBzkz0Y-2RsOD3wb5ZpXqGGDqdyVXGSMi3cHIDuIpnFTfFQek9nhb2ihm5lOxILH9sm4msRVG4AcOF4hwKJCLlKj8YZQeRPhOiliGP0l2r24YBacEm0VDRuVCeJS01oa2151kbpnfL1jG5LtcUGNwAAAAEs3fMdAA")
+                        "حط كود تيرمكس الي تريد تنصبله هنا")
 SESSION = os.environ.get("TERMUX",
-                        "AQEEODcAgJ2YhFOVyw4Sr_3wYXFzgIxdYtovgbPqHTaVXlp286F5i2vEPtem35I-HijIb77NNv3TLRd0Y4K_rQXYBCmTrD0tytqYKjdlupQ2AicZz5Lj5fkyNsVNdtjAWJgROyZkHhyYOJtWNvFeu94AFx8t1t0xi9prHYKS9bNeTTBneSeBNe8uSP9IZfNYmB2C1kSzwFBzkz0Y-2RsOD3wb5ZpXqGGDqdyVXGSMi3cHIDuIpnFTfFQek9nhb2ihm5lOxILH9sm4msRVG4AcOF4hwKJCLlKj8YZQeRPhOiliGP0l2r24YBacEm0VDRuVCeJS01oa2151kbpnfL1jG5LtcUGNwAAAAEs3fMdAA")
+                        "حط كود تيرمكس الي تريد تنصبله هنا")
 token = os.environ.get("TOKEN",
-                      "5700874811:AAEgZ4OKHOLJ98Va_OO7Qr-Uuxg1f0BcFrg")
+                      "حط توكن بوت الي تريد تنصبله هنا")
 fifthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
 ispay2 = ['yes']
 bot.start()
+#كل الكودات تنحط بين ال ""
